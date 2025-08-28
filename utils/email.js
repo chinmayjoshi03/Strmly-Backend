585dd8995c2b5da304bd392f39ce3c1e0ef5f8d6
Rohith BN <138430569+RohithBN@users.noreply.github.com>
2025-08-28 21:16:51 +0530

    Feat/detection (#152)
    * admin controllers for detection
    
    * admin api fixes
    
    * added admin panel frontend origins to server
    
    * comment gifting admin api
    
    * create wallet upon registration and sort video likes based on creation
    
    * api fixes
    
    * add series in video upload
    
    * user following communities api
    
    * fixed joined community api and populated creator pass details in user videos
    
    * add missing bracket
    
    * api fixes
    
    * moved add creator logic when user purchases community fee
    
    * email config changes

--- utils/email.js
+++ utils/email.js
@@ -4,6 +4,9 @@ const crypto=require('crypto');
 const createTransporter=()=>{
     return nodemailer.createTransport({
         service: 'gmail',
+        host: 'smtp.gmail.com',
+        port:587,
+        secure:false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
